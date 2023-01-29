9c6a4e9060f973da735cff9520e317a409979a8d
ericklrobles <eglrobles@gmail.com>
2023-01-29 13:59:11 -0800

    commit

---
+++ measure.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+#include <string.h>
+
+int main(void){
+    printf("some measurement converter coming soonTM");
+    return 0;
+}
