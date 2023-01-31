20fd319684697e0e0eba1a456a2dd4ca77461f5d
ericklrobles <eglrobles@gmail.com>
2023-01-30 21:51:46 -0800

    commit1

--- measure.c
+++ measure.c
@@ -5,14 +5,18 @@
 #include <string.h>
 
 int main(void){
-    int pounds, kilos, miles, kilometers;
+    int pounds, miles, kilometers, kilos;
     char input;
+    char new_input;
     
     
      printf("Enter A for pounds to kilo converter or Enter B for miles to kilometers converter\n");
      scanf(" %c", &input);
      if (input == 'A'){
-        printf("you have chosen the pounds to kilo converter\n");
+        printf("Enter how many pounds you would like to convert to\n");
+        scanf(" %d", pounds);
+        new_input = (pounds);
+        
      }
      if (input =='B'){
         printf("you have chosen the miles to kilometers converter\n");
