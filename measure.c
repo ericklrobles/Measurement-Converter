ea7528cfada6f12108e4d4d31cab82f4468da35d
ericklrobles <eglrobles@gmail.com>
2023-01-30 21:23:55 -0800

    commit

--- measure.c
+++ measure.c
@@ -5,6 +5,19 @@
 #include <string.h>
 
 int main(void){
-    printf("some measurement converter coming soonTM");
+    int pounds, kilos, miles, kilometers;
+    char input;
+    
+    
+     printf("Enter A for pounds to kilo converter or Enter B for miles to kilometers converter\n");
+     scanf(" %c", &input);
+     if (input == 'A'){
+        printf("you have chosen the pounds to kilo converter\n");
+     }
+     if (input =='B'){
+        printf("you have chosen the miles to kilometers converter\n");
+     }
+    
+    
     return 0;
 }
