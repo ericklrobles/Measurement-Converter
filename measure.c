ed84110de24f4f52debb859a20e69ddc84be6b91
ericklrobles <eglrobles@gmail.com>
2023-02-05 12:12:17 -0800

    commit5

--- measure.c
+++ measure.c
@@ -3,7 +3,8 @@
 #include <ctype.h>
 #include <math.h>
 #include <string.h>
-#define ptokilo
+#define ptokilo 0.454
+#define milestokm 1.6
 
 int main(void){
     int pounds, miles, kilometers, kilos;
@@ -16,14 +17,14 @@ int main(void){
      if (input == 'A'){
         printf("Enter how many pounds you would like to convert to\n");
         scanf(" %d", &pounds);
-        new_input = (pounds * 0.454);
+        new_input = (pounds * ptokilo);
         printf(" The %d amount of pounds has converted into %.2f kilograms!\n", pounds, new_input);  
      }
      
      if (input =='B'){
         printf("Enter how many miles you would like to convert to\n");
         scanf(" %d", &miles);
-        new_input = (miles * 1.6);
+        new_input = (miles * milestokm);
         printf("The %d amount of miles has converted into %.2f km!", miles, new_input);
      }
     
